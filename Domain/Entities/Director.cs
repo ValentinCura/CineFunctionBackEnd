4e4959fc502f9b31b2fdce74297072e25b891b7b
Valentin Cura <valencuran@gmail.com>
2024-08-07 22:07:01 -0300

    Proyect Introduction

---
+++ Domain/Entities/Director.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Director
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+        
+
+    }
+}
---
+++ Domain/Entities/Film.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Film
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Description { get; set; }
+
+        [Required]
+        [ForeignKey("IdDirector")]
+        public int IdDirector { get; set; }
+        public Director Director { get; set; }
+
+    }
+}
