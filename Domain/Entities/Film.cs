c0a4989fe5b27eeca10fbb410fc616982efd84c0
mateocaranta <mateocaranta@gmail.com>
2024-08-09 20:09:34 -0300

    Images

--- Domain/Entities/Film.cs
+++ Domain/Entities/Film.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Domain.Entities
 {
@@ -27,6 +28,9 @@ namespace Domain.Entities
 
         [Required]
         public string Origin { get; set; }
+
+        [Required]
+        public string ImageUrl {  get; set; } 
 
     }
 }
