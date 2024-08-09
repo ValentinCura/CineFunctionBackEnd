1364d08f7fadb2ef8dcd373cb692a8da5dedefb1
mateocaranta <mateocaranta@gmail.com>
2024-08-09 18:22:42 -0300

    Context creation

---
+++ Infraestructure/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Director>? Directors { get; set; } 
+        public DbSet<Film>? Films { get; set; }
+        public DbSet<MovieScreening>? MoviesScreening { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) 
+        {
+
+        }
+    }
+}
