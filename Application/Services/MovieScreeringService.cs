81a7ef9ba4931973cd0d431127d07daffd3726c5
Nicolas Arrastia <nicolasarrastia04@gmail.com>
2024-08-09 18:54:28 -0300

    create services

--- Application/Services/MovieScreeringService.cs
+++ Application/Services/MovieScreeringService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Application.Interfaces;
+using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,30 @@ using System.Threading.Tasks;
 
 namespace Application.Services
 {
-    internal class MovieScreeringService
-    {
-    }
+    //public class MovieScreeringService : IMovieScreeringServices
+    //{
+    //   // private readonly IRepositoryBase
+    //    public MovieScreeringService() { }
+
+    //    public async Task<IEnumerable<MovieScreening>> GetAllMovieScreening() 
+    //    { 
+
+    //    }
+    //    public async Task<MovieScreening> GetMovieScreeningById(int id) 
+    //    { 
+        
+    //    }
+    //    public async Task<MovieScreening> CreateMovieScreening(MovieScreening movieScreening, int sellerId) 
+    //    {
+            
+    //    }
+    //    public async Task DeleteMovieScreening(int id) 
+    //    { 
+
+    //    }
+    //    public async Task UpdateMovieScreening(int id, MovieScreening movieScreening) 
+    //    { 
+
+    //    }
+    //}
 }
