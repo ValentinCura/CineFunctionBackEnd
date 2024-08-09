7a0fcb74a0ca8db82bc4fe9d5ceafed65ffba7c6
Valentin Cura <valencuran@gmail.com>
2024-08-09 19:01:33 -0300

    fixes

---
+++ Infraestructure/Repositories/MovieScreeningRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repositories
+{
+    public class MovieScreeningRepository : BaseRepository<MovieScreening> , IMovieScreeningRepository
+    {
+        private readonly ApplicationContext _context;
+        public MovieScreeningRepository(ApplicationContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
